91493d5f02fc7329f78406005c6b6ea949ea1f57
Chris Harvey <chharvey@icloud.com>
2017-12-18 01:10:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,7 +83,7 @@ class Student
       self.new_from_db(row)
     end
   end
-  
+
   def self.first_x_students_in_grade_10(num)
     sql = <<-SQL
     SELECT *
@@ -95,7 +95,7 @@ class Student
       self.new_from_db(row)
     end
   end
-  
+
   def self.first_student_in_grade_10
     sql = <<-SQL
     SELECT *
