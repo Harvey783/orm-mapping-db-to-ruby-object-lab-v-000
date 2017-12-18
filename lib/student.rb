f5e8943709e5edb4a5a29cc5474ff3455e59569d
Chris Harvey <chharvey@icloud.com>
2017-12-18 00:57:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,6 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
     new_student = Student.new
     new_student.id = row[0]
     new_student.name = row[1]
@@ -11,8 +10,6 @@ class Student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
     sql = <<-SQL
     SELECT *
     FROM students;
@@ -21,11 +18,9 @@ class Student
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
-end
+  end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
     sql = <<-SQL
     SELECT *
     FROM students
@@ -33,7 +28,7 @@ end
     SQL
 
     DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
+      new_from_db(row)
     end.first
   end
 
@@ -62,27 +57,25 @@ end
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
-end
 
-def self.count_all_students_in_grade_9
-  sql = <<-SQL
-  SELECT COUNT(*)
-  FROM students
-  WHERE grade = 9;
-  SQL
-  DB[:conn].execute(sql).map do |row|
-    self.new_from_db(row)
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+    SELECT COUNT(*)
+    FROM students
+    WHERE grade = 9;
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
-end
 
-def self.students_below_12th_grade
-  sql = <<-SQL
-  SELECT COUNT(*)
-  FROM students
-  WHERE grade < 12;
-  SQL
-  DB[:conn].execute(sql).map do |row|
-    self.new_from_db(row)
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT COUNT(*)
+    FROM students
+    WHERE grade < 12;
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
-end
-end
