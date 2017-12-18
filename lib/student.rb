03b2d11ca6ccea36afbe4c161fd6a210163bb1d6
Chris Harvey <chharvey@icloud.com>
2017-12-18 00:45:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,12 +15,13 @@ class Student
     # remember each row should be a new instance of the Student class
     sql = <<-SQL
     SELECT *
-    FROM students
+    FROM students;
     SQL
 
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
   end
+end
 
   def self.find_by_name(name)
     # find the student in the database given a name
